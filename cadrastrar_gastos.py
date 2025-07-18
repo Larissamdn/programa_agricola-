ac69404e09dfa24d45fc272620bb61ce048b4f17
Larissa Maria <203013871+Larissamdn@users.noreply.github.com>
2025-07-18 14:55:42 +0000

    testes

--- cadrastrar_gastos.py
+++ cadrastrar_gastos.py
@@ -1,1 +1,22 @@
-tomate {'ara' : 5.0, 'mudas': 100.0, 'insumos': 30.0, 'mao de obra': 60.0, 'energia': 600.0 }
+
+c_tomate = {'arr' : 5.0, 'mudas': 100.0, 'insumos': 30.0, 'mao de obra': 60.0, 'energia': 600.0 }
+b_tomate = [3.00, 500]
+
+c_banana = {'arr' : 5.0, 'mudas': 125.0, 'insumos': 50.0, 'mao de obra': 60.0, 'energia': 600.0 }
+b_banana = [2.00, 400]
+
+c_alho = {'arr' : 5.0, 'mudas': 90.0, 'insumos': 50.0, 'mao de obra': 60.0, 'energia': 100.0 }
+b_alho = [1.00, 100]
+
+c_goiaba = {'arr' : 5.0, 'mudas': 100.0, 'insumos': 30.0, 'mao de obra': 60.0, 'energia': 600.0 }
+b_goiaba = [3.00,300]
+
+c_aborbora = {'arr' : 5.0, 'mudas': 90.0, 'insumos': 100.0, 'mao de obra': 620.0, 'energia': 600.0 }
+b_aborbora = [5.00, 500]
+
+hect = int(input("Digite a quantidade de hectares: "))
+prod = input("Digite o produtos: ")
+
+if prod == "tomate":
+    c_tomate * hect 
+
